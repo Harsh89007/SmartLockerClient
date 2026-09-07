090ad56d42f2ba43a3a56f6c2933fdf82e537d12
Harsh89007 <151430440+Harsh89007@users.noreply.github.com>
2026-09-07 15:33:27 +0530

    Create BootReceiver.kt

---
+++ app/src/main/java/com/harshit/client/BootReceiver.kt
@@ -0,0 +1,21 @@
+package com.harshit.client
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            val serviceIntent = Intent(context, LockService::class.java)
+            
+            // एंड्रॉइड के नए वर्ज़न में सर्विस स्टार्ट करने का तरीका
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(serviceIntent)
+            } else {
+                context.startService(serviceIntent)
+            }
+        }
+    }
+}
